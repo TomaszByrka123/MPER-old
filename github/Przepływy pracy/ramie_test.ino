44561e73910c5d132ae51a1b6a92e52c67a4ade1
TomaszByrka123 <tombyrka123@gmail.com>
2023-01-05 19:57:48 +0100

    Update ramie_test.ino

--- github/Przepływy pracy/ramie_test.ino
+++ github/Przepływy pracy/ramie_test.ino
@@ -1,1 +1,21 @@
+void loop(/* arguments */) {
+  /* code */
+}
+voi(/* arguments */) {
+  /* code */
+}
+class klasa {
+private:
+  /* data */
 
+public:
+  klasa (argumen ts);
+  virtual ~klasa ();
+};
+
+int name(/* arguments */) {
+  /* code */
+  return 0;
+}
+
+int
